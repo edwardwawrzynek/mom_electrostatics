3ca59e2b30839fb451ff2f9f489f0c067f6ed89d
Edward Wawrzynek <edward@wawrzynek.com>
2024-04-25 22:36:28 -0600

    code for error plot generation

--- mom.m
+++ mom.m
@@ -4,19 +4,68 @@
 
 function mom
     set(0,'defaultTextInterpreter','latex');
-    mesh = endpointsMesh(table2array(readtable("culogo.csv")), 500, BasisFunctions.Pulse);
-    mesh.points = mesh.points / 200.0;
+    % mesh = endpointsMesh(table2array(readtable("culogo.csv")), 500, BasisFunctions.Pulse);
+    %mesh.points = mesh.points / 200.0;
     
-    %mesh = circleMesh(100, BasisFunctions.Triangle);
-    mesh = Mesh([0 0; 1 1; 2 2], BasisFunctions.Triangle);
-    %mesh = squareMesh(100, BasisFunctions.Triangle);
-    mesh.weights = [0 1 0 ];
+    pts = 200;
+    mesh_delta = circleMesh(pts, BasisFunctions.Delta);
+    mesh_pulse = circleMesh(pts, BasisFunctions.Pulse);
+    mesh_tri = circleMesh(pts, BasisFunctions.Triangle);
 
-    mesh.plotMesh();
+    %mesh = Mesh([0 0; 1 1; 2 2], BasisFunctions.Delta);
+    %mesh = squareMesh(100, BasisFunctions.Pulse);
+    %mesh = roundCornerMesh(100, 0.2, BasisFunctions.Pulse);
+    %mesh.weights = [0 1 0 ];
 
-    %mesh = mesh.solve(1.0);
-    mesh.plotCharge();
-    mesh.plotVoltage(1.7, 100);
+    %mesh.plotMesh();
+    
+    mesh_delta = mesh_delta.solve(1.0);
+    mesh_pulse = mesh_pulse.solve(1.0);
+    mesh_tri = mesh_tri.solve(1.0);
+    
+    sigma = mean(mesh_pulse.weights);
+    
+    n = 200;
+
+    [v_tri, ~, ~, ~, ~] = mesh_tri.computeVoltage(1.5, n);
+    [v_pulse, ~, ~, ~, ~] = mesh_pulse.computeVoltage(1.5, n);
+    [v_delta, ~, ~, ~, ~] = mesh_delta.computeVoltage(1.5, n);
+    [v_anal, axleft, axright, ayleft, ayright] = mesh_tri.computeVoltageHoop(1.5, sigma, n);
+    
+    max_error = max([max(abs(v_pulse-v_anal), [], "all") max(abs(v_delta-v_anal), [], "all") max(abs(v_tri-v_anal), [], "all")]);
+
+    f = figure(1);
+    s = subplot(1,3,1);
+    mesh_delta.plotMap(abs(v_delta-v_anal), axleft, axright, ayleft, ayright, n);
+    colormap("hot");
+    caxis(s, [0 max_error]);
+    title("Delta");
+
+    s = subplot(1,3,2);
+    mesh_pulse.plotMap(abs(v_pulse-v_anal), axleft, axright, ayleft, ayright, n);
+    colormap("hot");
+    caxis(s, [0 max_error]);
+    title("Pulse");
+
+    s = subplot(1,3,3);
+    mesh_tri.plotMap(abs(v_tri-v_anal), axleft, axright, ayleft, ayright,200);
+    colormap("hot");
+    caxis(s, [0 max_error]);
+    title("Triangle");
+
+    h = axes(f, "visible", "off");
+    h.Title.Visible = "on";
+    h.XLabel.Visible = "on";
+    h.YLabel.Visible = "on";
+    xlabel(h, "x [m]");
+    ylabel(h, "y [m]");
+    title("Absolute Error [V]");
+
+
+    c = colorbar(h, 'Position', [0.93 0.168 0.022 0.7]);
+    colormap(c,'hot');
+    caxis(h, [0 max_error]);
+    
 
     %BasisFunctions.Pulse.evaluateVoltage([2 2], [1 1], [0 0], [0 0], 0.01);
 end
@@ -155,5 +204,19 @@ function mesh = squareMesh(n, basis)
             [zeros(n,1) linspace(1,1/n,n).']
         ];
     
+    mesh = Mesh(pts, basis);
+end
+
+% construct a mesh with rounded corner
+function mesh = roundCornerMesh(n, radius, basis)
+    pts = [ [linspace(-1+1/n,-1/n, n).' ones(n,1) .* radius]; ...
+            [cos(linspace(pi/2,0,floor(n*pi*radius/2))).' .* radius sin(linspace(pi/2,0,floor(n*pi*radius/2))).' .* radius]; ...
+            [ones(n,1) .* radius linspace(-1/n,-1+1/n, n).']; ...
+            [cos(linspace(0,-pi/2,floor(n*pi*radius/2))).' .* radius sin(linspace(0,-pi/2,floor(n*pi*radius/2))).' .* radius - ones(floor(n*pi*radius/2),1)]; ...
+            [linspace(-1/n,-1+1/n, n).' ones(n,1) .* -(1+radius)]; ...
+            [(cos(linspace(-pi/2,-pi,floor(n*pi*radius/2))).' .* radius -ones(floor(n*pi*radius/2),1)) sin(linspace(-pi/2,-pi,floor(n*pi*radius/2))).' .* radius - ones(floor(n*pi*radius/2),1)]; ...
+            [ones(n,1) .* -(1+radius) linspace(-1+1/n,-1/n, n).']; ...
+            [(cos(linspace(pi,pi/2,floor(n*pi*radius/2))).' .* radius -ones(floor(n*pi*radius/2),1)) sin(linspace(pi,pi/2,floor(n*pi*radius/2))).' .* radius]; ...
+            ];
     mesh = Mesh(pts, basis);
 end
